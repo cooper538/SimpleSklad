7561bcd3fe593efdd2cb8a7422baa1604d2a4b38
vrabec0853 <tomas.starenko@gmail.com>
2019-06-13 12:52:45 +0200

    Add EmailSender service
    get secret by secret-manager tool
    have to have SendGrid account
    
    source:
    https://docs.microsoft.com/cs-cz/aspnet/core/security/authentication/accconfirm?view=aspnetcore-2.2&tabs=visual-studio

--- SimpleSklad/Startup.cs
+++ SimpleSklad/Startup.cs
@@ -6,18 +6,45 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using SimpleSklad.Models;
+using SimpleSklad.Services;
 
 namespace SimpleSklad
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            // https://ardalis.com/aspnet-core-inmemorydatabase-upgrade-breaking-change
+            string dbName = Guid.NewGuid().ToString();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase(dbName));
+            //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.Configure<AuthMessageSenderOptions>(Configuration);
+
             services.AddMvc();
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,7 +58,7 @@ namespace SimpleSklad
             // For wwwroot directory
             app.UseStaticFiles();
 
-            // Add support for node_modules but only during development **temporary**
+            // HACK: Add support for node_modules but only during development **temporary**
             if (env.IsDevelopment())
             {
                 app.UseStaticFiles(new StaticFileOptions()
