faf598b481c3c398b0c17413bad5b69aef16a738
vrabec0853 <tomas.starenko@gmail.com>
2019-06-13 14:13:21 +0200

    Add ApplicationDbContext and configure service ef in Startup.cs

---
+++ SimpleSklad/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SimpleSklad.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
--- SimpleSklad/Startup.cs
+++ SimpleSklad/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using SimpleSklad.Models;
 using SimpleSklad.Services;
 
 namespace SimpleSklad
@@ -29,6 +30,11 @@ namespace SimpleSklad
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // https://ardalis.com/aspnet-core-inmemorydatabase-upgrade-breaking-change
+            string dbName = Guid.NewGuid().ToString();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase(dbName));
+            //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));+
 
             services.AddTransient<IEmailSender, EmailSender>();
             services.Configure<AuthMessageSenderOptions>(Configuration);
